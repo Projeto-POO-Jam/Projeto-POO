9cf3859a84669a1f284e4300066a226945e624aa
Eduardo Franco <eduardo.f.seco@gmail.com>
2025-06-07 16:30:05 -0300

    :recycle: refactor: Ajustes na arquitetura visual

--- jam/src/main/resources/static/js/Login.js
+++ jam/src/main/resources/static/js/Login.js
@@ -1,44 +1,14 @@
 $(document).ready(function() {
     var $form = $("#loginForm");
-    var $emailInput = $("#email");
+    var $usernameInput = $("#username");
     var $passwordInput = $("#password");
-    var $emailError = $("#emailError");
-
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    function validateField(fieldName) {
-        if (fieldName === "email") {
-            var emailValue = $.trim($emailInput.val());
-            if (!emailValue) {
-                $emailError.text("O e-mail é obrigatório.");
-                return false;
-            } else if (!emailRegex.test(emailValue)) {
-                $emailError.text("Formato de e-mail inválido.");
-                return false;
-            } else {
-                $emailError.text("");
-                return true;
-            }
-        }
-    }
-
-    $emailInput.on("blur", function() {
-        validateField("email");
-    });
 
     $form.on("submit", function(event) {
-        var isEmailValid = validateField("email");
-
-        if (!isEmailValid) {
-            event.preventDefault();
-            return;
-        }
-
         event.preventDefault();
 
         apiRequest('POST', 'perform_login', {
-                username: "Eduardo Franco",
-                password: "Teste1234."
+                username: $.trim($usernameInput.val()),
+                password: $passwordInput.val(),
         }, 'application/x-www-form-urlencoded')
             .then(function(resultado) {
                 console.log('Usuário logado:', resultado.data);
