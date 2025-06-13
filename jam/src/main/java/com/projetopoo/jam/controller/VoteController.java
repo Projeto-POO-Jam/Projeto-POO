97e565e819ad3b4da2c4b7a522ed6474a53eb208
igorskeff <igorskeff@users.noreply.github.com>
2025-06-13 13:26:56 -0300

    :sparkles: feat: Adição da rota para pegar o voto do usuario em um jogo

--- jam/src/main/java/com/projetopoo/jam/controller/VoteController.java
+++ jam/src/main/java/com/projetopoo/jam/controller/VoteController.java
@@ -1,22 +1,15 @@
 package com.projetopoo.jam.controller;
 
-import com.projetopoo.jam.dto.UserResponseDTO;
 import com.projetopoo.jam.dto.VoteRequestDTO;
-import com.projetopoo.jam.exception.UserValidationException;
-import com.projetopoo.jam.model.User;
-import com.projetopoo.jam.service.UserService;
+import com.projetopoo.jam.dto.VoteResponseDTO;
 import com.projetopoo.jam.service.VoteService;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.security.Principal;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/votes")
@@ -24,15 +17,21 @@ public class VoteController {
     @Autowired
     private VoteService voteService;
 
+    @GetMapping
+    public ResponseEntity<?> findVote(@RequestBody VoteRequestDTO voteRequest, Principal principal) {
+        try {
+            VoteResponseDTO voteResponseDTO = voteService.findVote(voteRequest, principal.getName());
+            return ResponseEntity.status(HttpStatus.CREATED).body(voteResponseDTO);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
     @PostMapping
     public ResponseEntity<?> toggleVote(@RequestBody VoteRequestDTO voteRequest, Principal principal) {
         try {
-            boolean vote = voteService.toggleVote(voteRequest.getGameId(), principal.getName());
-            if (vote) {
-                return ResponseEntity.status(HttpStatus.CREATED).build();
-            } else {
-                return ResponseEntity.noContent().build();
-            }
+            VoteResponseDTO voteResponseDTO = voteService.toggleVote(voteRequest, principal.getName());
+            return ResponseEntity.status(HttpStatus.CREATED).body(voteResponseDTO);
         } catch (EntityNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
--- jam/src/main/java/com/projetopoo/jam/service/VoteService.java
+++ jam/src/main/java/com/projetopoo/jam/service/VoteService.java
@@ -1,28 +1,18 @@
 package com.projetopoo.jam.service;
 
-import com.projetopoo.jam.dto.UserResponseDTO;
+import com.projetopoo.jam.dto.VoteRequestDTO;
+import com.projetopoo.jam.dto.VoteResponseDTO;
 import com.projetopoo.jam.exception.UserValidationException;
-import com.projetopoo.jam.model.Comment;
 import com.projetopoo.jam.model.Game;
-import com.projetopoo.jam.model.User;
 import com.projetopoo.jam.model.Vote;
 import com.projetopoo.jam.repository.GameRepository;
 import com.projetopoo.jam.repository.UserRepository;
 import com.projetopoo.jam.repository.VoteRepository;
-import com.projetopoo.jam.util.ImageUtil;
-import com.projetopoo.jam.util.UpdateUtil;
 import jakarta.persistence.EntityNotFoundException;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -36,25 +26,51 @@ public class VoteService {
     private VoteRepository voteRepository;
 
     @Transactional
-    public boolean toggleVote(Long gameId, String identifier) {
+    public VoteResponseDTO findVote(VoteRequestDTO voteRequestDTO, String identifier) throws UserValidationException {
+        VoteResponseDTO voteResponseDTO = new VoteResponseDTO();
+
+        Vote vote = getVote(voteRequestDTO, identifier);
+        Optional<Vote> optionalVote = voteRepository.findByVoteUserAndVoteGame(vote.getUser(), vote.getGame());
+
+        if(optionalVote.isPresent()) {
+            voteResponseDTO.setVoted(true);
+            return voteResponseDTO;
+        } else {
+            voteResponseDTO.setVoted(false);
+            return voteResponseDTO;
+        }
+    }
+
+    @Transactional
+    public VoteResponseDTO toggleVote(VoteRequestDTO voteRequestDTO, String identifier) {
+        VoteResponseDTO voteResponseDTO = new VoteResponseDTO();
+
+        Vote vote = getVote(voteRequestDTO, identifier);
+        Optional<Vote> optionalVote = voteRepository.findByVoteUserAndVoteGame(vote.getUser(), vote.getGame());
+
+        if(optionalVote.isPresent()) {
+            voteRepository.delete(optionalVote.get());
+            voteResponseDTO.setVoted(false);
+            return voteResponseDTO;
+        } else {
+            voteRepository.save(vote);
+            voteResponseDTO.setVoted(true);
+            return voteResponseDTO;
+        }
+    }
+
+    @Transactional
+    public Vote getVote(VoteRequestDTO voteRequestDTO, String identifier) {
         Vote vote = new Vote();
 
         vote.setUser(userRepository.findByIdentifier(identifier));
 
-        Optional<Game> optionalGame = gameRepository.findByGameId(gameId);
+        Optional<Game> optionalGame = gameRepository.findByGameId(voteRequestDTO.getGameId());
         if (optionalGame.isEmpty()) {
-            throw new EntityNotFoundException("Jogo com o ID " + gameId + " não encontrado.");
+            throw new EntityNotFoundException("Jogo com o ID " + voteRequestDTO.getGameId() + " não encontrado.");
         }
         vote.setGame(optionalGame.get());
-
-        Optional<Vote> optionalVote = voteRepository.findByVoteUserAndVoteGame(vote.getUser(), vote.getGame());
-        if(optionalVote.isPresent()) {
-            voteRepository.delete(optionalVote.get());
-            return false;
-        } else {
-            voteRepository.save(vote);
-            return true;
-        }
+        return vote;
     }
 
 }
