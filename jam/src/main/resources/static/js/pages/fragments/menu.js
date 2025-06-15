06abb353532e0d875cc5ca3b0581e367159cce41
Eduardo Franco <eduardo.f.seco@gmail.com>
2025-06-15 17:36:00 -0300

    :sparkles: feat: Implementando carregamento skeleton

--- jam/src/main/resources/static/js/pages/fragments/menu.js
+++ jam/src/main/resources/static/js/pages/fragments/menu.js
@@ -1,17 +1,23 @@
 import { fetchCurrentUser } from '../../services/userService.js';
 import { bindDataFields } from '../../common/bindDataFields.js';
+import { applySkeleton, removeSkeleton } from '../../common/skeleton.js';
 
 $(function() {
+    const root = '.site-header';
+    applySkeleton(root);
+
     fetchCurrentUser()
         .then(data => {
-            bindDataFields(data);
+            bindDataFields(data, root);
+            removeSkeleton(root);
         })
         .catch(err => {
             console.error('Erro ao buscar usuário:', err);
+            removeSkeleton(root);
             if (err.status === 401) {
                 window.location.href = '/login';
             } else {
                 alert('Não foi possível carregar os dados do usuário.');
             }
-        });
+        })
 });
