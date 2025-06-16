1d94e432e173817a79f8f713f150e8ca720a91bd
Eduardo Franco <eduardo.f.seco@gmail.com>
2025-06-16 20:15:02 -0300

    :sparkles: feat: Implementando pagina view Jam Inicial
    Ainda necessários vários ajustes

--- jam/src/main/java/com/projetopoo/jam/controller/PageController.java
+++ jam/src/main/java/com/projetopoo/jam/controller/PageController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PageController {
@@ -16,6 +17,11 @@ public class PageController {
 
     @GetMapping("/registerJam")
     public String registerJam() {return "pages/registerJam";}
+
+    @GetMapping("/jams/{id}")
+    public String viewJam(@PathVariable Long id) {
+        return "pages/viewJam";
+    }
 
     @GetMapping("/login")
     public String login() {
