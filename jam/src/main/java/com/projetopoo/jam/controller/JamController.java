534820786f951e92518a8c5e7633ba975625acf1
igorskeff <igorskeff@users.noreply.github.com>
2025-06-17 16:09:32 -0300

    :sparkles: feat: Adição da rota para inserir jam

---
+++ jam/src/main/java/com/projetopoo/jam/controller/JamController.java
@@ -0,0 +1,35 @@
+package com.projetopoo.jam.controller;
+
+import com.projetopoo.jam.dto.CommentRequestDTO;
+import com.projetopoo.jam.dto.CommentResponseDTO;
+import com.projetopoo.jam.dto.JamRequestDTO;
+import com.projetopoo.jam.service.CommentService;
+import com.projetopoo.jam.service.JamService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/jams")
+public class JamController {
+    @Autowired
+    private JamService jamService;
+
+    @PostMapping(consumes = { "multipart/form-data" })
+    public ResponseEntity<?> createJam(JamRequestDTO jamRequestDTO, Principal principal) {
+        try {
+            jamService.createJam(jamRequestDTO, principal.getName());
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (IllegalArgumentException | IOException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+}
---
+++ jam/src/main/java/com/projetopoo/jam/service/JamService.java
@@ -0,0 +1,59 @@
+package com.projetopoo.jam.service;
+
+import com.projetopoo.jam.dto.CommentResponseDTO;
+import com.projetopoo.jam.dto.JamRequestDTO;
+import com.projetopoo.jam.model.Comment;
+import com.projetopoo.jam.model.Jam;
+import com.projetopoo.jam.model.User;
+import com.projetopoo.jam.repository.JamRepository;
+import com.projetopoo.jam.repository.UserRepository;
+
+import com.projetopoo.jam.util.ImageUtil;
+import jakarta.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import org.modelmapper.ModelMapper;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class JamService {
+
+    @Autowired
+    private JamRepository jamRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    private static final String UPLOAD_DIRECTORY = "src/main/resources/static/upload/jam";
+
+    @Transactional
+    public void createJam(JamRequestDTO jamRequestDTO, String identifier) throws IOException {
+
+        Jam jam = modelMapper.map(jamRequestDTO, Jam.class);
+
+        jam.setJamUser(userRepository.findByIdentifier(identifier));
+
+        String uuid = UUID.randomUUID().toString();
+        String directoryCover = UPLOAD_DIRECTORY + "/" + uuid + "/cover";
+        jam.setJamCover(ImageUtil.createImage(jamRequestDTO.getJamCover(), directoryCover, "/upload/jam/" + uuid + "/cover/"));
+
+        String directoryWallpaper = UPLOAD_DIRECTORY + "/" + uuid + "/wallpaper";
+        jam.setJamWallpaper(ImageUtil.createImage(jamRequestDTO.getJamWallpaper(), directoryWallpaper, "/upload/jam/" + uuid + "/wallpaper/"));
+
+        String directoryBanner = UPLOAD_DIRECTORY + "/" + uuid + "/banner";
+        jam.setJamBanner(ImageUtil.createImage(jamRequestDTO.getJamBanner(), directoryBanner, "/upload/jam/" + uuid + "/banner/"));
+
+        jamRepository.save(jam);
+    }
+
+}
