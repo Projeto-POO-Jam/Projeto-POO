53741888ec1b7d85cd27aa514cee30153dbb401a
Eduardo Franco <eduardo.f.seco@gmail.com>
2025-06-19 22:32:28 -0300

    :ok_hand: style: Pequenos ajustes

--- jam/src/main/resources/static/js/pages/registerJam.js
+++ jam/src/main/resources/static/js/pages/registerJam.js
@@ -12,16 +12,9 @@ $(function() {
             theme: 'classic',
             default: defaultColor,
 
-            swatches: [
-                '#f44336', '#e91e63', '#9c27b0', '#673ab7',
-                '#3f51b5', '#2196f3', '#03a9f4', '#00bcd4',
-                '#009688', '#4caf50', '#8bc34a', '#cddc39',
-                '#ffeb3b', '#ffc107', '#ff9800', '#795548'
-            ],
-
             components: {
                 preview: true,
-                opacity: true,
+                opacity: false,
                 hue: true,
                 interaction: {
                     hex: true,
