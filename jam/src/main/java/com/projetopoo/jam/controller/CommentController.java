268924bcdae49c654bff97bfa559256f8ed02671
igorskeff <igorskeff@users.noreply.github.com>
2025-06-23 22:45:43 -0300

    :books: docs: Adição da documentação nas rotas de comentarios

--- jam/src/main/java/com/projetopoo/jam/controller/CommentController.java
+++ jam/src/main/java/com/projetopoo/jam/controller/CommentController.java
@@ -3,6 +3,12 @@ package com.projetopoo.jam.controller;
 import com.projetopoo.jam.dto.comment.CommentRequestDTO;
 import com.projetopoo.jam.dto.comment.CommentResponseDTO;
 import com.projetopoo.jam.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,17 +21,23 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/comments")
+@Tag(
+        name = "Comentários",
+        description = "Endpoints relacionados aos comentários de jogos"
+)
 public class CommentController {
     @Autowired
     private CommentService commentService;
 
-    @GetMapping("/list/{gameId}")
-    public ResponseEntity<List<CommentResponseDTO>> findCommentsList(@PathVariable Long gameId) {
-        List<CommentResponseDTO> comments = commentService.findCommentsList(gameId);
-        return ResponseEntity.ok(comments);
-    }
-
     @PostMapping
+    @Operation(
+            summary = "Cria um novo comentário",
+            description = "Adiciona um comentário a um jogo. Requer autenticação.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Comentário criado com sucesso", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Acesso negado", content = @Content)
+    })
     public ResponseEntity<?> createComment(@RequestBody CommentRequestDTO commentRequestDTO, Principal principal) {
         try {
             commentService.createComment(commentRequestDTO, principal.getName());
@@ -35,7 +47,32 @@ public class CommentController {
         }
     }
 
+    @GetMapping("/list/{gameId}")
+    @Operation(
+            summary = "Lista comentários de um jogo",
+            description = "Retorna todos os comentários de um jogo específico.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Comentários listados com sucesso",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = CommentResponseDTO.class)))
+    })
+    public ResponseEntity<List<CommentResponseDTO>> findCommentsList(@PathVariable Long gameId) {
+        List<CommentResponseDTO> comments = commentService.findCommentsList(gameId);
+        return ResponseEntity.ok(comments);
+    }
+
     @DeleteMapping("/{commentId}")
+    @Operation(
+            summary = "Exclui um comentário",
+            description = "Exclui um comentário pelo seu ID. Apenas o autor do comentário pode realizar esta ação. Requer autenticação.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Comentário excluído com sucesso", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Acesso negado. O usuário não é o autor do comentário.", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Comentário não encontrado", content = @Content)
+    })
     public ResponseEntity<?> deleteComment(@PathVariable Long commentId, Principal principal) {
         try {
             commentService.deleteComment(commentId, principal.getName());
