6c0e1d82effb94a4529a562faff540fdb07c0028
igorskeff <igorskeff@users.noreply.github.com>
2025-06-23 22:36:10 -0300

    :heavy_plus_sign: build: Adição das dependencias do Swagger

---
+++ jam/src/main/java/com/projetopoo/jam/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.projetopoo.jam.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("JAM API")
+                        .version("v1.0")
+                        .description("API para a plataforma de Game Jams. Permite criar e gerenciar Jams, submeter jogos, votar e comentar."));
+    }
+}
